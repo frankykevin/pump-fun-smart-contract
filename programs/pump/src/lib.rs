2e03dae35d16b755e0da73523c30196592d75ab1
frankykevin <frankykevin.development@gmail.com>
2024-12-13 20:46:46 +0000

    delete old instructions

--- programs/pump/src/lib.rs
+++ programs/pump/src/lib.rs
@@ -14,27 +14,4 @@ declare_id!("7RiUM3T5TE6VrqKE9ekPfn6SZNQ7Z7FEmQCyoXytdEVD");
 pub mod pump {
     use super::*;
 
-    pub fn initialize(ctx: Context<InitializeCurveConfiguration>, fee: f64) -> Result<()> {
-        instructions::initialize(ctx, fee)
-    }
-
-    // pub fn create_pool(ctx: Context<CreateLiquidityPool>) -> Result<()> {
-    //     instructions::create_pool(ctx)
-    // }
-
-    pub fn add_liquidity(
-        ctx: Context<AddLiquidity>,
-        amount_one: u64,
-        amount_two: u64,
-    ) -> Result<()> {
-        instructions::add_liquidity(ctx, amount_one, amount_two)
-    }
-
-    pub fn remove_liquidity(ctx: Context<RemoveLiquidity>, shares: u64) -> Result<()> {
-        instructions::remove_liquidity(ctx, shares)
-    }
-
-    pub fn swap(ctx: Context<Swap>, amount: u64, style: u64) -> Result<()> {
-        instructions::swap(ctx, amount, style)
-    }
 }
