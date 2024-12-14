4e54a5b3cd9d5daec1feec3d09aa65be357df502
frankykevin <frankykevin.development@gmail.com>
2024-12-14 20:56:22 +0000

    add config instruction

--- programs/pump/src/lib.rs
+++ programs/pump/src/lib.rs
@@ -14,4 +14,9 @@ declare_id!("7RiUM3T5TE6VrqKE9ekPfn6SZNQ7Z7FEmQCyoXytdEVD");
 pub mod pump {
     use super::*;
 
+    //  called by admin to set global config
+    //  need to check the signer is authority
+    pub fn configure(ctx: Context<Configure>, new_config: states::Config) -> Result<()> {
+        ctx.accounts.process(new_config)
+    }
 }
